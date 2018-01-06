ea049028b639864e86563323c3bf7a2aba0136df
Windjy E. Jean <Yuugami@users.noreply.github.com>
2018-01-06 16:50:34 -0500

    Fixed "Include" bug

--- SMProj/index.php
+++ SMProj/index.php
@@ -11,13 +11,11 @@ PHP Social Media Project
     </head>
     <body>
         <?php include ("./CommonFiles/Header.php"); ?>
-
         <div class="container-fluid" style="padding-top: 20px; padding-left: 40px">
             <h1>Welcome to the Algonquin Social Media Website</h1>
             <p>If you have never used this website before, <a href="./NewUser.php">sign-up</a> is required.</p>
             <p>If you have already signed up, you can <a href="./Login.php">login</a> now.
         </div>
-
         <?php include ("./CommonFiles/Footer.php"); ?>
     </body>
 </html>
