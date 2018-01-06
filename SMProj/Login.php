d3f98c126d9dea0cf1776e9d3081c36431610e7f
Windjy E. Jean <Yuugami@users.noreply.github.com>
2018-01-06 18:56:28 -0500

    Login.php Initial Commit

--- SMProj/Login.php
+++ SMProj/Login.php
@@ -5,13 +5,37 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<head>
+    <meta charset="UTF-8" />
+    <title>Algonquin Social Media Website</title>
+</head>
+<body>
+    <?php include ("./CommonFiles/Header.php"); ?>
+
+    <h1>Log In</h1>
+    <div class="wrapper">
+        <p>
+            You need to
+            <a href="#">sign up</a>
+            if you're a new user.
+        </p>
+        <form action="/" method="post">
+            <div class="dataEntryContainer">
+                <span>User ID:</span>
+                <input type="text" name="name" value="" />
+                <br /><br />
+                <span>Password:</span>
+                <input type="text" name="name" value="" />
+                <br /><br />
+                <div class="formButtons">
+                    <input type="submit" name="name" value="Submit" />
+                    <input type="reset" name="name" value="Clear" />
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+    <?php include ("./CommonFiles/Footer.php"); ?>
+</body>
 </html>
