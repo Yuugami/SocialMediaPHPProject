ce1e858fca032be4ef6424ed43b52b8adcb088c2
sshliu <liu00390@algonquinlive.com>
2018-01-07 17:13:02 -0500

    Added LoggedIn User Information

--- SMProj/AddAlbum.php
+++ SMProj/AddAlbum.php
@@ -8,13 +8,17 @@ PHP Social Media Project
 <body>
     <?php include ("./CommonFiles/Header.php"); ?>
     <div class="container">
-        <h1 style="text-align: center">Create New Album</h1><br>
-        <p class="text-center">Welcome <b>(insert name here)</b>! (Not you? Change user <a href="(insertlinkhere)">here</a>.)</p><br>
-
+        <h1 style="text-align: center">Create New Album</h1>
+        <br>
+        <p class="text-center">
+            Welcome <b><?php echo "$_SESSION[LoggedInUserName]"; ?></b>! 
+            (Not you? Change users <a href="<?php echo $directoryPrefix; ?>/Logout.php">here</a>.)
+        </p>
+        <br>
         <form class="form-horizontal" id="depositForm" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="form-group">
-                <label for="title" class="col-lg-3 col-lg-offset-1 control-label" style="text-align: left">Title</label>
-                <div class="col-lg-4">
+                <label for="title" class="col-lg-2 col-lg-offset-1 control-label" style="text-align: left">Title</label>
+                <div class="col-lg-5">
                     <input type="text" class="form-control" id="title" name="title" value="<?php echo $title; ?>">
                 </div>
                 <div class="col-lg-4 text-danger">
@@ -22,8 +26,8 @@ PHP Social Media Project
                 </div>                        
             </div>
             <div class="form-group">
-                <label for="accessibility" class="col-lg-3 col-lg-offset-1 control-label" style="text-align: left">Accessibility:</label>
-                <div class="col-lg-4">
+                <label for="accessibility" class="col-lg-2 col-lg-offset-1 control-label" style="text-align: left">Accessibility:</label>
+                <div class="col-lg-5">
                     <select class="form-control" id="accessibility" name="accessibility" value="<?php echo $accessibility; ?>">
                         <option style="display:none">Please Select Level of Access</option>
                         <option value="private">Accessible Only by Owner</option>
@@ -35,8 +39,8 @@ PHP Social Media Project
                 </div>
             </div>
             <div class="form-group">
-                <label for="description" class="col-lg-3 col-lg-offset-1 control-label" style="text-align: left">Description:</label>
-                <div class="col-lg-4">
+                <label for="description" class="col-lg-2 col-lg-offset-1 control-label" style="text-align: left">Description:</label>
+                <div class="col-lg-5">
                     <textarea rows="6" type="" class="form-control" id="description" name="description" value="<?php echo $description; ?>"></textarea>
                 </div>
                 <div class="col-lg-4 text-danger">
