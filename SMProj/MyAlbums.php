72d1ac3ed1980ac0da314489bbc954d1fdfdf083
Windjy E. Jean <Yuugami@users.noreply.github.com>
2018-01-06 23:14:55 -0500

    MyAlbums Complete

--- SMProj/MyAlbums.php
+++ SMProj/MyAlbums.php
@@ -9,18 +9,35 @@ and open the template in the editor.
     <div class="container">
         <h1>My Albums</h1>
         <p>
-            Welcome <?php echo $studentName?>! (not you? change user <a href="<?php echo $directoryPrefix;?>/Logout.php">here</a>
+            Welcome <?php echo $studentName?>! (not you? change user <a href="<?php echo $directoryPrefix;?>/Logout.php">here</a>)
         </p>
         <table class="table">
             <tr>
-                <td>Title</td>
-                <td>Date Updated</td>
-                <td>Number of Pictures</td>
-                <td>Accessibilitiy</td>
-                <td></td>
+                <th>Title</th>
+                <th>Date Updated</th>
+                <th>Number of Pictures</th>
+                <th>Accessibility</th>
+                <th></th>
             </tr>
+            <?php
+            // Print All Albums
+            for ($i = 0; $i <= 5; $i++) {
+                echo <<< EOT
+                <tr>
+                <td>My China Trip</td>
+                <td>2017-09-04</td>
+                <td>14</td>
+                <td>
+                    <select class="form-control" id="accessibility" name="accessibility" value="<?php echo $accessibility; ?>">
+                        <option style="display:none">Please Select Number of Years</option>
+                        <option value="private">Accessible Only by Owner</option>
+                        <option value="shared">Accessible by Owner and Friends</option>
+                    </select>
+                <td><a href="#">delete</a></td>
+EOT;
+            }
+            ?>
         </table>
     </div>
-
     <?php include ("./CommonFiles/Footer.php"); ?>
 </body>
