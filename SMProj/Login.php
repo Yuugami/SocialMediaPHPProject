e849597fef5fb3435b69e0496068cb993563768f
Windjy E. Jean <Yuugami@users.noreply.github.com>
2018-01-07 13:49:44 -0500

    Login.php Minor Changes

--- SMProj/Login.php
+++ SMProj/Login.php
@@ -1,3 +1,4 @@
+<?php include ("./CommonFiles/Header.php"); ?>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -7,10 +8,10 @@ and open the template in the editor.
 <?php
     // If user is logged in, assign Student object to $LoggedInUser, otherwise redirect to login and die (self-executing function)
     //$LoggedInUser = isset($_SESSION["LoggedInUser"]) ? $_SESSION["LoggedInUser"] : (function() { header("Location: Login.php?returnUrl=".urlencode($_SERVER['REQUEST_URI'])); die();})();
+
 ?>
 
 <body>
-    <?php include ("./CommonFiles/Header.php"); ?>
     <div class="container">
         <h1>Log In</h1>
         <p>You need to <a href="<?php echo $directoryPrefix; ?>/NewUser.php">sign up</a> if you're a new user.</p>
@@ -18,14 +19,14 @@ and open the template in the editor.
             <div class="form-group">
                 <label for="inputUserID" class="col-lg-2 control-label" style="text-align: left">User ID:</label>
                 <div class="col-lg-3">
-                    <input type="text" class="form-control" id="inputUserID" name="id" placeholder="User ID" />
+                    <input type="text" class="form-control" id="inputUserID" name="inputUserID" placeholder="User ID" />
                 </div>
                 <div class="col-lg-4 text-danger"></div>
             </div>
             <div class="form-group">
                 <label for="inputPassword" class="col-lg-2 control-label" style="text-align: left">Password:</label>
                 <div class="col-lg-3">
-                    <input type="text" class="form-control" id="inputPassword" name="password" placeholder="Password" />
+                    <input type="text" class="form-control" id="inputPassword" name="inputPassword" placeholder="Password" />
                 </div>
                 <div class="col-lg-4 text-danger"></div>
             </div>
