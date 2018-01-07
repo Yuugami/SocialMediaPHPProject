578e3610aff742267f5b0bd5fb47528f38f9b9aa
alam0098 <alam0098@algonquinlive.com>
2018-01-07 15:22:05 -0500

    checkUserId & bug fix for LoginQuery

--- SMProj/SqlScripts/Queries.php
+++ SMProj/SqlScripts/Queries.php
@@ -10,8 +10,23 @@ function loginQuery($UserId, $Password) {
     $data = $pStatment->fetch();
 
     if ($data)
-        return data;
+        return $data;
     else
         return FALSE;
 }
+
+function checkUserId($UserId){
+    $dbConnection = parse_ini_file("db_connection.ini");
+    extract($dbConnection);
+    $myPdo = new PDO($dsn, $user, $password);
+    $sql = 'SELECT Name FROM Users WHERE UserId = :userId';
+    $pStatment = $myPdo->prepare($sql);
+    $pStatment->execute(array('userId' => $UserId));
+    $data = $pStatment->fetch();
+    if ($data)
+        return TRUE; // If User Exists
+    else
+        return FALSE; // If User does not exist
+}
+
 ?>
