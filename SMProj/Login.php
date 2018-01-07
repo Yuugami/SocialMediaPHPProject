1016b72513d71e94bc94fcbe367e8d128639da7d
Windjy E. Jean <Yuugami@users.noreply.github.com>
2018-01-07 13:59:02 -0500

    Header has the Session Start

--- SMProj/Login.php
+++ SMProj/Login.php
@@ -8,7 +8,12 @@ and open the template in the editor.
 <?php
     // If user is logged in, assign Student object to $LoggedInUser, otherwise redirect to login and die (self-executing function)
     //$LoggedInUser = isset($_SESSION["LoggedInUser"]) ? $_SESSION["LoggedInUser"] : (function() { header("Location: Login.php?returnUrl=".urlencode($_SERVER['REQUEST_URI'])); die();})();
+$loginError = false;
 
+if ($_POST) {
+    $studentID = $_POST["inputUserID"];
+    $password = $_POST["inputPassword"];
+}
 ?>
 
 <body>
