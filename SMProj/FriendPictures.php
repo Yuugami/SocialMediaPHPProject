4ee4687faefc1a3dd80533490c210ecfe56142c4
Windjy E. Jean <Yuugami@users.noreply.github.com>
2018-01-11 18:40:59 -0500

    Bugfix for FriendPictures

--- SMProj/FriendPictures.php
+++ SMProj/FriendPictures.php
@@ -59,6 +59,7 @@ if($_POST) {
         <h1><?php echo $_SESSION['friendYouAreVisiting']?>'s Pictures</h1>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <select class="form-control" id="albums" name="albums" onchange="reloadPage(this.value)">
+                <option value="" style="display:none">Please Select Album to Display</option>
                 <?php
                 foreach ($loggedInUsersFriendsAlbums as $anAlbum) {
                     if ($anAlbum[Album_Id] == $selectedAlbum[Album_Id]) {
